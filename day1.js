841e083a3f5f1f542d2be998c6ffb35d7969a047
eyerus123355 <talargeeyerus@gmail.com>
2026-06-12 04:43:26 -0700

    feat: complete day 1 assignment and practical loops

--- day1.js
+++ day1.js
@@ -23,3 +23,10 @@ rl.question('Please enter your age: ', (userInput) => {
     rl.close();
     process.exit(0);
 });
+// Change "Your Name" to your actual name
+const yourName = "eyu"; 
+
+// Using a for loop to print the name 10 times
+for (let i = 1; i <= 10; i++) {
+    console.log(`${i}. ${yourName}`);
+}
