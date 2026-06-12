d2fc4b60e531f023af1d756d15050f7d4875011c
eyerus123355 <talargeeyerus@gmail.com>
2026-06-12 04:28:29 -0700

    feat: complete day 1 assignment and practical loops

---
+++ day11.js
@@ -0,0 +1,7 @@
+// Change "Your Name" to your actual name
+const yourName = "eyu"; 
+
+// Using a for loop to print the name 10 times
+for (let i = 1; i <= 10; i++) {
+    console.log(`${i}. ${yourName}`);
+}
